29c9934972088108fb27c7ac96a666598737bcc5
georgeksaji <georgeksaji14@gmail.com>
2023-08-03 05:35:50 +0530

    Completed Staff Registration Page and Customer Registration including database(not evaluated)

---
+++ Customer_Login.php
@@ -0,0 +1,174 @@
+<html>
+<head>
+  <title>Customer Login</title>
+  <style>
+    body {
+      background-image: url("background1.jpg");
+      background-size: cover;
+      background-repeat: no-repeat;
+    }
+
+    .outercontainer {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+    }
+
+    .registration-box {
+    background-color: rgb(0,0,0,0.5);
+    padding: 18px;
+    border-radius: 10px;
+    width: fit-content;
+    text-align: center;
+    transition: 0.5s;
+    }
+
+    .registration-box:hover {
+      transform: scale(1.01);
+    
+    }
+
+    .registration-box-logo {
+      background-image: url(Picture3.png);
+      margin: auto;
+      background-size: contain;
+      background-repeat: no-repeat;
+      height:80px;
+      width:263px;
+
+    }
+
+    .registration-box-heading {
+      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+      margin: auto;
+      color: white;
+      margin-bottom: 20px;
+      font-weight: 500;
+      font-size: 180%;
+    }
+
+    .registration-box input[type="password"],
+    .registration-box input[type="email"]
+    {
+    width: 344px;
+    padding: 7px;
+    margin-left: 15px;
+    margin-right: 15px;
+    margin-bottom: 15px;
+    border-color: rgb(82,176,210);
+    background-color: rgb(82,176,210,0.1);
+    border-width: 1px;
+    border-style: double;
+    border-radius: 5px;
+    color: white;
+    text-align: initial;
+    }
+
+
+
+    .registration-box input[type="email"]:focus,
+    .registration-box input[type="password"]:focus
+    {
+      background-color: rgb(0,0,0,0.5);
+      outline: none;
+      border-color:rgba(145, 204, 234, 0.992);
+      box-shadow: 0 0 10px #9ecaed;
+    }
+
+    ::placeholder{
+      color: white;
+    }
+
+   label
+   {
+    color: rgba(255, 255, 255, 0.9);
+    text-align: left;
+    font-size: medium;
+    display: block;
+    margin-left: 20px;
+   }
+
+
+    .registration-form-left,
+    .registration-form-right
+    {
+      flex-basis: 45%; 
+    }
+
+    option
+    {
+      background-color:rgb(0,0,0,0.7);
+    }
+
+
+   .login-button {
+    background-color: rgb(255,216,21,0.6);
+    border-radius: 5px;
+    color: white;
+    width: 180px;
+    cursor: pointer;
+    margin: 20px;
+    padding: 8px 5px;
+    border: none;
+    font-size: 19px;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-weight: 500;
+    width: 241px;
+    }
+
+    .login-button:hover {
+      background-color: rgb(255,216,21,0.9);
+      transition: 0.2s;
+    }
+
+    .signup-button {
+    background-color: rgb(83,178,212,0.6);
+    border-radius: 5px;
+    color: white;
+    width: 180px;
+    cursor: pointer;
+    padding: 8px 5px;
+    border: none;
+    font-size: 19px;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-weight: 500;
+    width: 241px;
+    }
+
+    .signup-button:hover {
+      background-color: rgb(83,178,212,0.9);
+      transition: 0.2s;
+    }
+
+
+    p{
+      color: whitesmoke;
+    }
+
+  </style>
+  </head>
+<body>
+  <div class="outercontainer">
+    <div class="registration-box">
+      <div class="registration-box-logo"></div>
+      <div class="registration-box-heading">Login</div>
+      <form>
+        <div class="registration-form">
+          <div class="registration-form-left">
+            
+            <label for="email">Username</label>
+            <input type="email" id="email" name="email" placeholder="Email" required>
+              
+            <label for="passwordright">Password</label>
+            <input type="password" id="passwordright" name="passwordright" placeholder="Enter password" minlength="6" required title="Minimum 6 characters required.">
+        </div>
+        <button class="login-button">Login</button>
+      </form>
+       </div>
+    <div class="signup-redirect">
+      <p>Dont have an account? Register here.</p>
+      <a href="Customer_Sign_Up.html"><button class="signup-button" type="submit">Create your Account</button></a>
+    </div>
+  </div>
+</body>
---
+++ Customer_Sign_Up.php
@@ -0,0 +1,243 @@
+<?php
+include ("connection.php");
+
+if(isset($_POST['submit']))
+{
+  $firstName = $_POST['firstName'];
+  $lastName = $_POST['lastName'];
+  $email = $_POST['email'];
+  $gender = $_POST['gender'];
+  $phoneNumber = $_POST['phoneNumber'];
+  $houseName = $_POST['houseName'];
+  $street = $_POST['street'];
+  $district = $_POST['district'];
+  $state = $_POST['state'];
+  $pincode = $_POST['pincode'];
+  $passwordright = $_POST['passwordright'];
+  $passwordleft = $_POST['passwordleft'];
+
+  if($passwordleft == $passwordright)
+  {
+    $sql = "SELECT * FROM tbl_login WHERE Username = '$email'";
+    $result = mysqli_query($conn, $sql);
+    $num = mysqli_num_rows($result);
+    if($num > 0) 
+    {
+      echo "<script>alert('Username already exists!');</script>";
+    } 
+    else
+    {
+      $insert1= "INSERT INTO tbl_login(Username,Password,User_Type)VALUES('$email','$passwordleft','CU')";
+      mysqli_query($conn,$insert1);
+      $insert1= "INSERT INTO tbl_customer(Cust_ID,C_Username,Cust_Fname,Cust_Lname,Cust_Phone,Cust_Gender,Cust_Hname,Cust_Street,Cust_Dist,State_Ut,Cust_Pin)VALUES(generate_cust_id(),'$email','$firstName','$lastName','$phoneNumber','$gender','$houseName','$street','$district','$state','$pincode')";
+      mysqli_query($conn,$insert1);
+      echo "<script>alert('Staff registered successfully!');</script>";
+      //header("Location: Customer_Login.php");
+    }
+  }
+  else
+  {
+  echo "<script>alert('Passwords do not match!');</script>";
+  }
+}
+?>
+
+<html>
+<head>
+  <title>Sign Up</title>
+  <style>
+    body {
+      background-image: url("background1.jpg");
+      background-size: cover;
+      background-repeat: no-repeat;
+    }
+
+    .outercontainer {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+    }
+
+    .registration-box {
+    background-color: rgb(0,0,0,0.47);
+    padding: 18px;
+    border-radius: 10px;
+    width: fit-content;
+    text-align: center;
+    transition: 0.5s;
+    }
+
+    .registration-box:hover {
+      transform: scale(1.01);
+    
+    }
+
+    .registration-box-logo {
+      background-image: url(Picture3.png);
+      margin: auto;
+      background-size: contain;
+      background-repeat: no-repeat;
+      height:80px;
+      width:263px;
+
+    }
+
+    .registration-box-heading {
+      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+      margin: auto;
+      color: white;
+      margin-bottom: 20px;
+      font-weight: 500;
+      font-size: 180%;
+    }
+
+    .registration-form
+    {
+      display: flex;
+    }
+
+    .registration-box input[type="text"],
+    .registration-box input[type="email"],
+    .registration-box input[type="tel"],
+    .registration-box input[type="number"],
+    .registration-box input[type="password"],
+    .registration-box select {
+    width: 344px;
+    padding: 7px;
+    margin-left: 15px;
+    margin-right: 15px;
+    margin-bottom: 15px;
+    border-color: rgb(82,176,210);
+    background-color: rgb(82,176,210,0.1);
+    border-width: 1px;
+    border-style: double;
+    border-radius: 5px;
+    color: white;
+    text-align: initial;
+    }
+
+
+    .registration-box input[type="text"]:focus,
+    .registration-box input[type="email"]:focus,
+    .registration-box input[type="tel"]:focus,
+    .registration-box input[type="password"]:focus,
+    .registration-box input[type="number"]:focus
+    {
+      background-color: rgb(0,0,0,0.5);
+      outline: none;
+      border-color:rgba(145, 204, 234, 0.992);
+      box-shadow: 0 0 10px #9ecaed;
+    }
+
+    ::placeholder{
+      color: white;
+    }
+
+   label
+   {
+    color: rgba(255, 255, 255, 0.9);
+    text-align: left;
+    font-size: medium;
+    display: block;
+    margin-left: 15px;
+   }
+
+
+    .registration-form-left,
+    .registration-form-right
+    {
+      flex-basis: 45%; 
+    }
+
+    option
+    {
+      background-color:rgb(0,0,0,0.7);
+    }
+
+
+    .registration-box button {
+    background-color: rgb(255,216,21,0.6);
+    border-radius: 5px;
+    color: white;
+    opacity: 1px;
+    width: 180px;
+    cursor: pointer;
+    margin-top: 20px;
+    padding: 8px 5px;
+    border: none;
+    font-size: 19px;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-weight: 500;
+    width: 241px;
+    }
+
+
+    .registration-box button:hover {
+      background-color: rgb(255,216,21,0.9);
+      transition: 0.2s;
+      
+    }
+
+  </style>
+  </head>
+<body>
+  <div class="outercontainer">
+    <div class="registration-box">
+      <div class="registration-box-logo"></div>
+      <div class="registration-box-heading">Create Account</div>
+      <form action="" method="POST">
+        <div class="registration-form">
+          <div class="registration-form-left">
+              <label for="firstName">Your name</label>
+              <input type="text" id="firstName" name="firstName" placeholder="First name" required>
+
+              <label for="email">Email/Username</label>
+              <input type="email" id="email" name="email" placeholder="Email" required>
+
+              <label for="gender">Gender</label>
+              <select id="gender" name="gender" required>
+                <option value="" disabled selected hidden>Select gender</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="other">Other</option>
+              </select>
+            
+              <label for="address">Street</label>
+              <input type="text" id="address" name="street" placeholder="Street" required>
+
+              <label for="pincode">Pincode</label>
+              <input type="text" id="pincode" name="pincode" placeholder="Enter 6 digit pincode" pattern="[0-9]{6}" required title="Enter a valid pincode">
+              
+              <label for="passwordright">Create password</label>
+              <input type="password" id="passwordright" name="passwordright" placeholder="Enter password" minlength="4" required title="Minimum 4 characters required.">
+
+          </div>
+
+          <div class="registration-form-right">
+
+            <label for="lastName"><!--Enter your name:--><br></label>
+            <input type="text" id="lastName" name="lastName" placeholder="Last name" required>
+  
+            <label for="phoneNumber">Phone number</label>
+            <input type="tel" id="phoneNumber" name="phoneNumber" placeholder="Enter a 10 digit phone number" pattern="[0-9]{10}" required title="Enter a valid phone number">
+
+            <label for="houseName">House name</label>
+            <input type="text" id="houseName" name="houseName" placeholder="House, apartment, suit, etc." required>
+          
+            <label for="district">District</label>
+            <input type="text" id="district" name="district" placeholder="District" required>
+
+            <label for="state">State</label>
+            <input type="text" id="state" name="state" placeholder="State" required>
+            
+            <label for="passwordleft"><br></label>
+            <input type="password" id="passwordleft" name="passwordleft" placeholder="Confirm password" minlength="4" required title="Minimum 4 characters required.">
+            </div>
+        </div>
+        <button type="submit" name="submit">Sign Up</button>
+      </form>
+    </div>
+  </div>
+</body>
+  </html>
